1ad2edfa8620a9b206ed0219e1f4f25e5761ff35
Gleb <mamaka.s.pistoletom@gmail.com>
2025-01-09 13:14:17 +0300

    09.12-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./styles/App.css";
+import { useState } from "react";
+import PostItem from "./Componets/PostItem";
+import Counter from "./Componets/Counter";
+import ClassCounter from "./Componets/ClassCouter";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Description" },
+    { id: 2, title: "JavaScript 2", body: "Description" },
+    { id: 3, title: "JavaScript 3", body: "Description" },
+  ]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {posts.map((post) => (
+        <PostItem post={post} key={post.id} />
+      ))}
+    </div>
+  );
 }
-
-export default App
+export default App;
