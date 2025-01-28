4765a18fc8d92ad69bcfdc7d8a5b8f18c1de3e38
Gleb <mamaka.s.pistoletom@gmail.com>
2025-01-28 14:54:34 +0300

    usememo-search-components-final

--- src/App.jsx
+++ src/App.jsx
@@ -16,31 +16,25 @@ function App() {
 
     const sortedPosts = useMemo(() => {
         console.log('function worked correctly');
-        if (selectedSort) {
-            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
         }
         return posts;
-    }, [selectedSort, posts]);
+    }, [filter.sort, posts]);
     const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter((post) => post.title.toLowerCase().includes(searchQuery));
-    }, [searchQuery, sortedPosts]);
+        return sortedPosts.filter((post) => post.title.toLowerCase().includes(filter.query));
+    }, [filter.query, sortedPosts]);
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
     };
     const removePost = (post) => {
         setPosts(posts.filter((p) => p.id !== post.id));
     };
-    const sortPosts = (sort) => {
-        setSelectedSort(sort);
-    };
     return (
         <div className="App">
             <PostForm create={createPost} />
             <hr style={{ margin: '15px 0' }} />
-            <PostFilter />
-            <div>
-                <PostList posts={sortedPosts} />
-            </div>
+            <PostFilter filter={filter} setFilter={setFilter} />
             {sortedAndSearchedPosts.length ? (
                 <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Lists of items'} />
             ) : (
