061520d491aa48ae9d14d867b10a1cff9699af70
Gleb <mamaka.s.pistoletom@gmail.com>
2025-01-29 11:40:27 +0300

    fetching-api-commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import PostFilter from './Componets/PostFilter.jsx';
 import MyModal from './Componets/UI/MyModal/MyModal.jsx';
 import MyButton from './Componets/UI/button/MyButton.jsx';
 import { usePosts } from './hooks/usePosts.js';
-import axios from 'axios';
 import PostService from './API/PostService.js';
 import Loader from './Componets/UI/Loader/Loader.jsx';
+import { useFetching } from './hooks/useFetching.js';
 
 function App() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({ sort: '', query: '' });
     const [modal, setModal] = useState(false);
-    const [isPostsLoading, setIsPostsLoading] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll();
+        setPosts(posts);
+    });
     useEffect(() => {
         fetchPosts();
     }, []);
@@ -25,20 +27,12 @@ function App() {
         setPosts([...posts, newPost]);
         setModal(false);
     };
-    async function fetchPosts() {
-        setIsPostsLoading(true);
-        setTimeout(async () => {
-            const posts = await PostService.getAll();
-            setPosts(posts);
-            setIsPostsLoading(false);
-        }, 1000);
-    }
+
     const removePost = (post) => {
         setPosts(posts.filter((p) => p.id !== post.id));
     };
     return (
         <div className="App">
-            <button onClick={fetchPosts}>get</button>
             <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
                 Create Post
             </MyButton>
@@ -49,6 +43,7 @@ function App() {
 
             <hr style={{ margin: '15px 0' }} />
             <PostFilter filter={filter} setFilter={setFilter} />
+            {postError && <h1>Error happened{postError}</h1>}
             {isPostsLoading ? (
                 <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}>
                     <Loader />
