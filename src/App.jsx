a90109d5ecd9ee12267437c0f6c25ab10f2daef4
Gleb <mamaka.s.pistoletom@gmail.com>
2025-01-29 10:55:18 +0300

    animations-loader

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,31 @@ import MyModal from './Componets/UI/MyModal/MyModal.jsx';
 import MyButton from './Componets/UI/button/MyButton.jsx';
 import { usePosts } from './hooks/usePosts.js';
 import axios from 'axios';
+import PostService from './API/PostService.js';
+import Loader from './Componets/UI/Loader/Loader.jsx';
 
 function App() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({ sort: '', query: '' });
     const [modal, setModal] = useState(false);
+    const [isPostsLoading, setIsPostsLoading] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
     useEffect(() => {
         fetchPosts();
     }, []);
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
         setModal(false);
     };
     async function fetchPosts() {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        setPosts(response.data);
+        setIsPostsLoading(true);
+        setTimeout(async () => {
+            const posts = await PostService.getAll();
+            setPosts(posts);
+            setIsPostsLoading(false);
+        }, 1000);
     }
     const removePost = (post) => {
         setPosts(posts.filter((p) => p.id !== post.id));
@@ -41,7 +49,13 @@ function App() {
 
             <hr style={{ margin: '15px 0' }} />
             <PostFilter filter={filter} setFilter={setFilter} />
-            <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Lists of items'} />
+            {isPostsLoading ? (
+                <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}>
+                    <Loader />
+                </div>
+            ) : (
+                <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Lists of items'} />
+            )}
         </div>
     );
 }
