017b80c162667084b7d0f3b48e339c85eedae819
Gleb <mamaka.s.pistoletom@gmail.com>
2025-01-29 16:37:51 +0300

    react-router-dom-created

--- src/Componets/PostItem.jsx
+++ src/Componets/PostItem.jsx
@@ -5,14 +5,12 @@ const PostItem = (props) => {
         <div className="post">
             <div className="post__content">
                 <strong>
-                    {props.number}.{props.post.title}
+                    {props.post.id}.{props.post.title}
                 </strong>
                 <div>{props.post.body}</div>
             </div>
             <div className="post__btns">
-                <MyButton onClick={() => props.remove(props.post)}>
-                    remove
-                </MyButton>
+                <MyButton onClick={() => props.remove(props.post)}>remove</MyButton>
             </div>
         </div>
     );
