1921f2dd568d52a494196f62004dca2a22e742e0
Gleb <mamaka.s.pistoletom@gmail.com>
2025-01-30 14:14:37 +0300

    usehistory-navigation-post-by-id

---
+++ src/pages/PostIdPage.jsx
@@ -0,0 +1,30 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useFetching } from '../hooks/useFetching.js';
+import PostService from '../API/PostService.js';
+import Loader from '../Componets/UI/Loader/Loader.jsx';
+const PostIdPage = () => {
+    const params = useParams();
+    const [post, setPost] = useState({});
+    const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+        const response = await PostService.getByID(id);
+        setPost(response.data);
+    });
+    useEffect(() => {
+        fetchPostById(params.id);
+    }, []);
+    return (
+        <div>
+            <h1>Вы открыли страницу {params.id}</h1>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <div>
+                    {post.id},{post.title}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default PostIdPage;
