52a722aa7364c8228c0c1d7e8064363265c10fa3
Gleb <mamaka.s.pistoletom@gmail.com>
2025-01-31 16:58:42 +0300

    routes-finished-auth

--- src/Componets/UI/Navbar/Navbar.jsx
+++ src/Componets/UI/Navbar/Navbar.jsx
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom';
+import MyButton from '../button/MyButton.jsx';
+import { useContext } from 'react';
+import { AuthContext } from '../../../context/index.jsx';
 
 const Navbar = () => {
+    const { isAuth, setIsAuth } = useContext(AuthContext);
     return (
         <div className="navbar">
+            <MyButton onClick={setIsAuth(false)}></MyButton>
             <div className="navbar__links">
                 <Link to="about">about us</Link>
                 <Link to="posts">posts</Link>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,18 @@
 import MyInput from '../Componets/UI/input/MyInput.jsx';
 import MyButton from '../Componets/UI/button/MyButton.jsx';
+import { useContext } from 'react';
+import { AuthContext } from '../context/index.jsx';
 
 const Login = () => {
+    const { isAuth, setIsAuth } = useContext(AuthContext);
+    const login = (event) => {
+        event.preventDefault();
+        setIsAuth(true);
+    };
     return (
         <div>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={login}>
                 <MyInput type="text" placeholder="Login" />
                 <MyInput type="password" placeholder="Password" />
                 <MyButton>Enter</MyButton>
